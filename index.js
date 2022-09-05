284d9b2674a6b336011efc36068b5a1f2030c582
egeergull <ege.ergul@ug.bilkent.edu.tr>
2022-09-05 19:21:27 +0300

    backend imporved

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoute");
-
+const messagesRoutes = require("./routes/messagesRoute");
 const app = express();
+const socket = require("socket.io");
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
+app.use("/api/messages", messagesRoutes);
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -25,4 +27,21 @@ mongoose
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Served started on port ${process.env.PORT}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
 });
